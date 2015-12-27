194a64de91eacb93d5d733ce1b7f2b4640b66e66
Victor campos <victor.campos@visagio.com>
2015-12-27 14:19:48 -0200

    video part 1

---
+++ lib/phoenix_react/repo.ex
@@ -0,0 +1,3 @@
+defmodule PhoenixReact.Repo do
+  use Ecto.Repo, otp_app: :phoenix_react
+end
---
+++ web/static/js/components/commentBox.jsx
@@ -0,0 +1,11 @@
+var CommentBox = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+
+export default CommentBox
