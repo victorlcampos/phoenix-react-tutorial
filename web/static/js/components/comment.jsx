4a7e3fa3239a0f04df6889c5358a4baf912050f3
Victor campos <victor.campos@visagio.com>
2015-12-28 23:08:37 -0200

    adding comment

---
+++ web/static/js/components/comment.jsx
@@ -0,0 +1,14 @@
+let Comment = React.createClass({
+  render: function() {
+    return (
+      <div className="comment">
+        <h2 className="commentAuthor">
+          {this.props.author}
+        </h2>
+        {this.props.children}
+      </div>
+    );
+  }
+});
+
+export default Comment
