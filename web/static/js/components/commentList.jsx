1212ff9cf482d6c042c223750f092a3478e37eca
Victor campos <victor.campos@visagio.com>
2015-12-28 22:53:32 -0200

    add comment list

---
+++ web/static/js/components/commentList.jsx
@@ -0,0 +1,11 @@
+let CommentList = React.createClass({
+  render: function() {
+    return (
+      <div className="commentList">
+        Hello, world! I am a CommentList.
+      </div>
+    );
+  }
+});
+
+export default CommentList
