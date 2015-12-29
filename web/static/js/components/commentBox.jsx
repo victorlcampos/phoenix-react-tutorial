f3ac4d7cb8109760a28bdbd9452be65092d0630e
Victor campos <victor.campos@visagio.com>
2015-12-28 22:54:07 -0200

    import comment form and list into commentBox

--- web/static/js/components/commentBox.jsx
+++ web/static/js/components/commentBox.jsx
@@ -1,8 +1,13 @@
+import CommentList from './commentList'
+import CommentForm from './commentForm'
+
 var CommentBox = React.createClass({
   render: function() {
     return (
       <div className="commentBox">
-        Hello, world! I am a CommentBox.
+        <h1>Comments</h1>
+        <CommentList />
+        <CommentForm />
       </div>
     );
   }
