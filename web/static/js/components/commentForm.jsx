b3cddabfa4772da51f1b0c225c7fe61969c4f2c5
Victor campos <victor.campos@visagio.com>
2015-12-28 22:53:47 -0200

    add comment form

---
+++ web/static/js/components/commentForm.jsx
@@ -0,0 +1,11 @@
+let CommentForm = React.createClass({
+  render: function() {
+    return (
+      <div className="commentForm">
+        Hello, world! I am a CommentForm.
+      </div>
+    );
+  }
+});
+
+export default CommentForm
