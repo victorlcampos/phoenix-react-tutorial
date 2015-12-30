d79470dbdd2d6928ac7d34f80c2fe19ad9990971
Victor campos <victor.campos@visagio.com>
2015-12-29 23:39:01 -0200

    adding state to commentbox

--- web/static/js/components/commentBox.jsx
+++ web/static/js/components/commentBox.jsx
@@ -2,11 +2,14 @@ import CommentList from './commentList'
 import CommentForm from './commentForm'
 
 var CommentBox = React.createClass({
+  getInitialState: function() {
+    return {data: [{id: 1, author: "Pete Hunt", text: "This is one comment"},{id: 2, author: "Jordan Walke", text: "This is *another* comment"}]};
+  },
   render: function() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList />
+        <CommentList data={this.state.data} />
         <CommentForm />
       </div>
     );
--- web/static/js/components/commentList.jsx
+++ web/static/js/components/commentList.jsx
@@ -1,11 +1,19 @@
 import Comment from './comment'
 
 let CommentList = React.createClass({
+  commentNodes: function() {
+    return this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+      );
+    });
+  },
   render: function() {
     return (
       <div className="commentList">
-        <Comment author="Pete Hunt">This is one comment</Comment>
-        <Comment author="Jordan Walke">This is *another* comment</Comment>
+        {this.commentNodes()}
       </div>
     );
   }
